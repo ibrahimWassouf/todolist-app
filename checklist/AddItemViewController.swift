fa005d75e31c423875cabc77772dd577f00cae52
Ibrahim Wassouf <ib981347@dal.ca>
2022-10-26 23:51:30 -0300

    worked on add item view and view controller

--- checklist/AddItemViewController.swift
+++ checklist/AddItemViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,7 +25,34 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done(){
+        print(textField.text!);
         navigationController?.popViewController(animated: true)
     }
 
+    override func tableView(
+        _ tableView: UITableView,
+        willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+            return nil;
+        }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true);
+        textField.becomeFirstResponder();
+    }
+    
+    //MARK: - textField delegate
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        //figures out what the new text will be
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(
+            in: stringRange,
+            with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
 }
